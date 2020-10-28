0c0b099f372b5e265dfd27e9e7ddbc6f44905125
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-10-28 13:36:00 +0100

    Main Page fragment created

---
+++ app/src/main/java/com/codecool/fosterize/main_page/MainPageFragment.kt
@@ -0,0 +1,21 @@
+package com.codecool.fosterize.main_page
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.codecool.fosterize.R
+
+class MainPageFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
